2c10b243db719b720394b7eb707bb567cf5e8511
Rafael B <rafaeltec_@live.com>
2024-07-25 11:34:00 -0300

    Instação/configuração do Entity FrameWork e implementação do padrão Repository

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            //O Código acima elimina a opções de criar as configurações abaixo, uma por uma.
+            //builder.ApplyConfiguration(new CategoryConfiguration());
+            //builder.ApplyConfiguration(new ProductConfiguration());
+        }
+
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    internal class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _productContex;
+
+        public ProductRepository(ApplicationDbContext context)
+        {
+            _productContex = context;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            _productContex.Add(product);
+            await _productContex.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _productContex.Products.FindAsync(id);
+        }
+
+        public async Task<Product> GetProductCategoryAsync(int id)
+        {
+            //Eager loading
+            return await _productContex.Products.Include(c => c.Category).SingleOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            return await _productContex.Products.ToListAsync();
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            _productContex.Remove(product);
+            await _productContex.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            _productContex.Update(product);
+            await _productContex.SaveChangesAsync();
+            return product;
+        }
+    }
+}
